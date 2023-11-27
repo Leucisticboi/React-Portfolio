b13f1575289603087f251f1ec3da3a7509123c75
Leucisticboi <116482248+Leucisticboi@users.noreply.github.com>
2023-11-27 07:24:51 -0700

    Huh

--- server.cjs
+++ server.cjs
@@ -1,8 +1,13 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.static(path.resolve(__dirname, 'dist')));
 
